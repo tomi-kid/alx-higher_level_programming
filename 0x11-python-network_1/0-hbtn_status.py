a495745372ea736ccb070eac16c9508a75ccc975
tomi-kid <tomiketema209@gmail.com>
2023-07-02 03:49:45 -0700

    Fix The URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ fetches https://intranet.hbtn.io/status.
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
         content = resp.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
