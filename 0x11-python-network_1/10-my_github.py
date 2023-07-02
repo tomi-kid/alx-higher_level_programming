0aa8376ca57101d0da4ab674b63c9d13881592cd
tomi-kid <tomiketema209@gmail.com>
2023-07-02 08:31:40 -0700

    Fix Coding Problem

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,11 +4,11 @@ Python script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
 import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    r = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(r.json().get('id'))
+    response = requests.get('https://api.github.com/user',
+                            auth=requests.auth.HTTPBasicAuth
+                            (sys.argv[1], sys.argv[2]))
+    json = response.json()
+    print('{}'.format(json.get('id')))
